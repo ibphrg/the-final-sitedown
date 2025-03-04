0e643d982d072a2855dd5676d63d73f9b955d9e1
Ian Brown <ian.brown@powerhrg.com>
2025-03-04 18:19:30 -0500

    0.8 site update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const tankChange = document.getElementById("tankButton");
 const healChange = document.getElementById("healButton");
 const dpsChange = document.getElementById("dpsButton");
+const baseChange = document.getElementById("heidi");
 
 function changeTankClass(){
     document.getElementById("heidi").src = "img/Hydira/WAR.png";
@@ -14,6 +15,11 @@ function changeDpsClass(){
     document.getElementById("heidi").src = "img/Hydira/NIN.png";
 }
 
+function changeToBase(){
+    document.getElementById("heidi").src = "img/Hydira/BASE.png";
+}
+
 tankChange.addEventListener('click',changeTankClass);
 healChange.addEventListener('click',changeHealClass);
 dpsChange.addEventListener('click',changeDpsClass);
+baseChange.addEventListener('click', changeToBase);
