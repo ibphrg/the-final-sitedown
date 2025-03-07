81f278ec7928370716055bf7635aee7c3535a22b
Ian Brown <ian.brown@powerhrg.com>
2025-03-06 23:14:16 -0500

    Added clickable element to flame icon

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const tankChange = document.getElementById("tankButton");
 const healChange = document.getElementById("healButton");
 const dpsChange = document.getElementById("dpsButton");
 const baseChange = document.getElementById("heidi");
+const flameButt = document.getElementById("flameButton");
 
 function changeTankClass(){
     document.getElementById("heidi").src = "img/Hydira/WAR.png";
@@ -19,7 +20,12 @@ function changeToBase(){
     document.getElementById("heidi").src = "img/Hydira/BASE.png";
 }
 
+function buttAlert(){
+    alert('Have you heard of the critically acclaimed MMORPG Final Fantasy XIV? With an expanded free trial which you can play through the entirety of A Realm Reborn and the award-winning Heavensward expansion up to level 60 for free with no restrictions on playtime.');
+}
+
 tankChange.addEventListener('click',changeTankClass);
 healChange.addEventListener('click',changeHealClass);
 dpsChange.addEventListener('click',changeDpsClass);
 baseChange.addEventListener('click', changeToBase);
+flameButt.addEventListener('click', buttAlert);
